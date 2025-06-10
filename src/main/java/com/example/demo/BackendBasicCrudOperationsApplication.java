0a211ec5dd1ddb5e56bb3c74fef65c764d1cccea
mayank-nikte <mayanknikte@gmail.com>
2025-06-10 19:54:56 +0530

    first commit

---
+++ src/main/java/com/example/demo/BackendBasicCrudOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBasicCrudOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBasicCrudOperationsApplication.class, args);
+	}
+
+}
